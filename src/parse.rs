c63d7b66bc0e1c3e59395e7e17f252627717d8df
Filip Tomaśko <shader301270@gmail.com>
2022-06-16 15:13:41 +0200

    Move parsing to separate file

---
+++ src/parse.rs
@@ -0,0 +1,127 @@
+use std::collections::HashMap;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
+use chrono::DateTime;
+
+use serde::{Deserialize, Serialize};
+use serde_json;
+
+// https://stackoverflow.com/questions/44205435/how-to-deserialize-a-json-file-which-contains-null-values-using-serde
+// null values are either skipped (defaulted to unit tuple or are an Option)
+#[derive(Serialize, Deserialize, Debug)]
+struct Entry {
+    ts: String,
+    #[serde(skip_deserializing)]
+    username: (),
+    #[serde(skip_deserializing)]
+    platform: (),
+    ms_played: i32,
+    #[serde(skip_deserializing)]
+    ip_addr_decrypted: (),
+    #[serde(skip_deserializing)]
+    user_agent_decrypted: (),
+    master_metadata_track_name: Option<String>,
+    master_metadata_album_artist_name: Option<String>,
+    master_metadata_album_album_name: Option<String>,
+    spotify_track_uri: Option<String>,
+    #[serde(skip_deserializing)]
+    episode_name: (),
+    #[serde(skip_deserializing)]
+    episode_show_name: (),
+    #[serde(skip_deserializing)]
+    spotify_episode_uri: (),
+    #[serde(skip_deserializing)]
+    reason_start: (),
+    #[serde(skip_deserializing)]
+    reason_end: (),
+    #[serde(skip_deserializing)]
+    shuffle: (),
+    #[serde(skip_deserializing)]
+    skipped: (),
+    #[serde(skip_deserializing)]
+    offline: (),
+    #[serde(skip_deserializing)]
+    offline_timestamp: (),
+    #[serde(skip_deserializing)]
+    incognito_mode: (),
+}
+
+struct SongStruct {
+    ts: DateTime<chrono::Utc>,
+    ms_played: i32,
+    track: String,
+    album: String,
+    artist: String,
+}
+
+pub fn parse(paths: Vec<&str>) -> Vec<HashMap<String, String>> {
+    let u = read_entries_from_file(paths[0]).unwrap();
+    let mut v: Vec<HashMap<String, String>> = Vec::new();
+    let mut empty = v.clone();
+    for entry in u {
+        let new_hash = entry_struct_to_hashmap(entry);
+        let new = new_hash.clone();
+        match new_hash.get("track") {
+            Some(data) => {
+                if data == "n/a" {
+                    empty.push(new_hash)
+                }
+            }
+            None => panic!(),
+        }
+        v.push(new);
+    }
+
+    println!("{:?}\nNum of all entries: {}", v, v.len());
+
+    println!("Num of non-song? entries: {}", empty.len());
+
+    let ts = String::from("2016-07-21T01:02:07Z");
+    // RFC 3339 is basically ISO 8601
+    let n = DateTime::parse_from_rfc3339(&ts).unwrap();
+    println!("{}", n);
+
+    v
+}
+
+// https://docs.serde.rs/serde_json/fn.from_reader.html
+fn read_entries_from_file<P: AsRef<Path>>(path: P) -> Result<Vec<Entry>, Box<dyn Error>> {
+    // Open the file in read-only mode with buffer.
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    // Read the JSON contents of tshe file as an instance of `User`.
+    let full_entries = serde_json::from_reader(reader)?;
+
+    // Return the `User`.
+    Ok(full_entries)
+}
+
+fn entry_struct_to_hashmap(entry: Entry) -> HashMap<String, String> {
+    let mut a = HashMap::new();
+    a.insert("timestamp".to_string(), entry.ts);
+    a.insert("ms_played".to_string(), entry.ms_played.to_string());
+    a.insert(
+        "track".to_string(),
+        parse_option(entry.master_metadata_track_name),
+    );
+    a.insert(
+        "album".to_string(),
+        parse_option(entry.master_metadata_album_album_name),
+    );
+    a.insert(
+        "artist".to_string(),
+        parse_option(entry.master_metadata_album_artist_name),
+    );
+    a
+}
+
+fn parse_option(opt: Option<String>) -> String {
+    match opt {
+        Some(data) => data,
+        None => "n/a".to_string(),
+    }
+}
