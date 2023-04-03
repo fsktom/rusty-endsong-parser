7ca912d02c2718d424ad4a18aff867239ccf2167
Filip Tomaśko <shader301270@gmail.com>
2023-04-03 11:55:35 +0000

    Make saving and opening plots more compact

--- src/plot.rs
+++ src/plot.rs
@@ -26,58 +26,7 @@ pub fn single(trace: (Box<dyn Trace>, String)) {
     let layout = Layout::new().title(format!("<b>{title}</b>").as_str().into());
     plot.set_layout(layout);
 
-    // creates plots/ folder
-    std::fs::create_dir_all("plots").unwrap();
-
-    // opens the plot in the browser
-    match std::env::consts::OS {
-        // see https://github.com/igiagkiozis/plotly/issues/132#issuecomment-1488920563
-        "windows" => {
-            let path = format!(
-                "{}\\plots\\{}.html",
-                std::env::current_dir().unwrap().display(),
-                title
-            );
-            plot.write_html(path.as_str());
-            std::process::Command::new("explorer")
-                .arg(&path)
-                .output()
-                .unwrap();
-        }
-        "macos" => {
-            let path = format!(
-                "{}/plots/{}.html",
-                std::env::current_dir().unwrap().display(),
-                title
-            );
-            plot.write_html(path.as_str());
-            std::process::Command::new("open")
-                .arg(&path)
-                .output()
-                .unwrap();
-        }
-        _ => {
-            let path = format!(
-                "{}/plots/{}.html",
-                std::env::current_dir().unwrap().display(),
-                title
-            );
-            plot.write_html(path.as_str());
-
-            // https://doc.rust-lang.org/book/ch12-05-working-with-environment-variables.html
-            match std::env::var("BROWSER") {
-                Ok(browser) => {
-                    std::process::Command::new(browser)
-                        .arg(&path)
-                        .output()
-                        .unwrap();
-                }
-                Err(_) => {
-                    eprintln!("Your BROWSER environmental variable is not set!");
-                }
-            }
-        }
-    };
+    write_and_open_plot(&plot, title.as_str());
 }
 
 /// Compares two traces in a single plot in the `plots/` folder
@@ -93,58 +42,7 @@ pub fn compare(trace_one: (Box<dyn Trace>, String), trace_two: (Box<dyn Trace>, 
     let layout = Layout::new().title(format!("<b>{title}</b>").as_str().into());
     plot.set_layout(layout);
 
-    // creates plots/ folder
-    std::fs::create_dir_all("plots").unwrap();
-
-    // opens the plot in the browser
-    match std::env::consts::OS {
-        // see https://github.com/igiagkiozis/plotly/issues/132#issuecomment-1488920563
-        "windows" => {
-            let path = format!(
-                "{}\\plots\\{}.html",
-                std::env::current_dir().unwrap().display(),
-                title
-            );
-            plot.write_html(path.as_str());
-            std::process::Command::new("explorer")
-                .arg(&path)
-                .output()
-                .unwrap();
-        }
-        "macos" => {
-            let path = format!(
-                "{}/plots/{}.html",
-                std::env::current_dir().unwrap().display(),
-                title
-            );
-            plot.write_html(path.as_str());
-            std::process::Command::new("open")
-                .arg(&path)
-                .output()
-                .unwrap();
-        }
-        _ => {
-            let path = format!(
-                "{}/plots/{}.html",
-                std::env::current_dir().unwrap().display(),
-                title
-            );
-            plot.write_html(path.as_str());
-
-            // https://doc.rust-lang.org/book/ch12-05-working-with-environment-variables.html
-            match std::env::var("BROWSER") {
-                Ok(browser) => {
-                    std::process::Command::new(browser)
-                        .arg(&path)
-                        .output()
-                        .unwrap();
-                }
-                Err(_) => {
-                    eprintln!("Your BROWSER environmental variable is not set!");
-                }
-            }
-        }
-    };
+    write_and_open_plot(&plot, title.as_str());
 }
 
 /// Returns the dates of all occurrences of the `aspect`
@@ -170,4 +68,60 @@ pub fn find_dates<Asp: Music>(
         dates.push(user_input_date_parser("now").unwrap());
     }
     dates
+}
+
+/// Creates the plot .html in the plots/ folder and opens it in the browser
+fn write_and_open_plot(plot: &Plot, title: &str) {
+    // creates plots/ folder
+    std::fs::create_dir_all("plots").unwrap();
+
+    // opens the plot in the browser
+    match std::env::consts::OS {
+        // see https://github.com/igiagkiozis/plotly/issues/132#issuecomment-1488920563
+        "windows" => {
+            let path = format!(
+                "{}\\plots\\{}.html",
+                std::env::current_dir().unwrap().display(),
+                title
+            );
+            plot.write_html(path.as_str());
+            std::process::Command::new("explorer")
+                .arg(&path)
+                .output()
+                .unwrap();
+        }
+        "macos" => {
+            let path = format!(
+                "{}/plots/{}.html",
+                std::env::current_dir().unwrap().display(),
+                title
+            );
+            plot.write_html(path.as_str());
+            std::process::Command::new("open")
+                .arg(&path)
+                .output()
+                .unwrap();
+        }
+        _ => {
+            let path = format!(
+                "{}/plots/{}.html",
+                std::env::current_dir().unwrap().display(),
+                title
+            );
+            plot.write_html(path.as_str());
+
+            // https://doc.rust-lang.org/book/ch12-05-working-with-environment-variables.html
+            match std::env::var("BROWSER") {
+                Ok(browser) => {
+                    std::process::Command::new(browser)
+                        .arg(&path)
+                        .output()
+                        .unwrap();
+                }
+                Err(_) => {
+                    eprintln!("Your BROWSER environmental variable is not set!");
+                }
+            }
+        }
+    };
 }
